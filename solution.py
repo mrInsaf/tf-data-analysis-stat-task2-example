a1bc568cf780818562f49832bacf96d0bbd17291
mrInsaf <111053336+mrInsaf@users.noreply.github.com>
2023-04-06 19:36:42 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 
 from scipy.stats import norm
-from scipy.stats import t
 
 
 chat_id = 816831722 # Ваш chat ID, не меняйте название переменной
@@ -11,15 +10,16 @@ def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    distance = np.sqrt(x[:, 0]**2 + x[:, 1]**2)
-    alpha = 1 - p
-    x_mean = np.mean(distance)
-    print('x_mean is ', x_mean)
-    s = np.std(distance, ddof=1)
-    df = len(distance) - 1
-
-    t_value = t.ppf((1 + alpha) / 2, df)
-    a = t.interval(alpha, df, loc=x_mean, scale=s/np.sqrt(p))
-
-
-    return a
+    
+    # Устанавливаем количество бутстрэп-выборок
+    n_bootstrap_samples = 1000
+    
+    n = len(x)
+    bootstrapped_statistics = []
+    for i in range(n_bootstrap_samples):
+        bootstrap_sample = np.random.choice(x, n, replace=True)
+        bootstrapped_statistics.append(np.mean(bootstrap_sample))
+    alpha = (1 - p) / 2
+    lower_quantile = np.quantile(bootstrapped_statistics, alpha)
+    upper_quantile = np.quantile(bootstrapped_statistics, 1 - alpha)
+    return lower_quantile, upper_quantile
