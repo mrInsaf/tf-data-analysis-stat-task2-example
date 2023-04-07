196a9909b9419b5d28c411a34f24b4c6fbfc0eb5
mrInsaf <111053336+mrInsaf@users.noreply.github.com>
2023-04-07 17:50:42 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,15 +8,10 @@ chat_id = 816831722 # Ваш chat ID, не меняйте название пе�
 
 def solution(p: float, x: np.array) -> tuple:
     n = len(x)
-    x_mean = np.mean(x)
-    s_squared = np.sum((x - x_mean) ** 2) / (n - 1)
-
-    alpha = 1 - p  # уровень значимости
-    df = n - 1    # степень свободы
-
-    chi2_lower = chi2.ppf(alpha / 2, df)
-    chi2_upper = chi2.ppf(1 - alpha / 2, df)
-
-    ci_lower = np.sqrt((n - 1) * s_squared / chi2_upper)
-    ci_upper = np.sqrt((n - 1) * s_squared / chi2_lower)
-    return ci_lower, ci_upper
+    alpha = 1 - p
+    s = np.std(x, ddof=1)
+    chi2_lower = chi2.ppf(alpha/2, n-1)
+    chi2_upper = chi2.ppf(1-alpha/2, n-1)
+    lower = np.sqrt((n-1) * s**2 / chi2_upper)
+    upper = np.sqrt((n-1) * s**2 / chi2_lower)
+    return (lower, upper)
