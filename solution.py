5bfc7f594e358ed2ced2efa0a24fff87a77aac93
mrInsaf <111053336+mrInsaf@users.noreply.github.com>
2023-04-07 19:11:07 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,11 +7,13 @@ from scipy.stats import norm
 chat_id = 816831722 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    n = len(x)
     alpha = 1 - p
-    s = np.std(x, ddof=1)
-    chi2_lower = chi2.ppf(alpha/2, n-1)
-    chi2_upper = chi2.ppf(1-alpha/2, n-1)
-    lower = np.sqrt((n-1) * s**2 / chi2_upper)
-    upper = np.sqrt((n-1) * s**2 / chi2_lower)
-    return (lower, upper)
+    n = len(x)
+    mean = np.mean(x)
+    std_dev = np.std(x, ddof=1)
+    df = n - 1
+    
+    lower = df * np.square(std_dev) / chi2.ppf(1 - alpha/2, df)
+    upper = df * np.square(std_dev) / chi2.ppf(alpha/2, df)
+    
+    return (np.sqrt(lower), np.sqrt(upper))
